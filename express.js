1514ed7f71f20998e67157dbd65bfe79199d758a
Jay Idrees <idreesjay@gmail.com>
2021-06-11 04:35:53 -0400

    Added information regarding installation of nodemon

---
+++ express.js
@@ -0,0 +1,3 @@
+const express=require('express')
+
+const app=express(); // Here I am creating a new variable called app that is storing all the functions of express, so that later I can invoke them using object oriented programming
