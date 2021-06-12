22f7d7970dbd79377d754e89d5b36bb83a1e080f
Jay Idrees <idreesjay@gmail.com>
2021-06-12 01:42:15 -0400

    Added content about REST API

--- express.js
+++ express.js
@@ -1,3 +1,8 @@
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const app=express(); // Here I am creating a new variable called app that is storing all the functions of express, so that later I can invoke them using object oriented programming
+// with app.use allows us to use middlewares on requests. Each middleware will start separately with app.use
+
+
+app.listen(5000);
