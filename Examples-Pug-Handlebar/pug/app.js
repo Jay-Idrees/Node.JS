e0592298830cab3ffe0c45fbe9a6b847c120d3ff
Jay Idrees <idreesjay@gmail.com>
2021-07-02 20:19:50 -0400

    Added code to import modules, using bodyparser in express for the pug app

---
+++ Examples-Pug-Handlebar/pug/app.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const express=require('express');
+const pug=require('pug');
+
+
+const app=express();
+
+// This is specifying the template engine to be used for rendering pug html pages. Here "pug" exactly matches the name of the module
+app.set('view engine','pug');
+// This line is defining the folder where the pug files are located
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',(req,res,next)=>{
+    res.render('index',)
+})
+
+
