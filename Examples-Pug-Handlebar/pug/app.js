06128f1dd880cf3c03ac599c067ba7219ea0d199
Jay Idrees <idreesjay@gmail.com>
2021-07-04 18:01:35 -0400

    successfully trouble shooted errors in the pug files and learnt that improper code spacing can give errors and the comments syntax is different from javascript

--- Examples-Pug-Handlebar/pug/app.js
+++ Examples-Pug-Handlebar/pug/app.js
@@ -1,9 +1,10 @@
-const bodyParser = require('body-parser');
 const express=require('express');
-
-const users=[]
+const bodyParser = require('body-parser');
 
 const app=express();
+const users=[];
+
+
 
 // This is specifying the template engine to be used for rendering pug html pages. Here "pug" exactly matches the name of the module
 app.set('view engine','pug');
@@ -12,21 +13,25 @@ app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', (req,res,next)=>{
+app.get('/',(req,res,next)=>{
+    // Anything tagged with # in 
     res.render('index', {pageTitle:'Add User'});
 });
 
 app.get('/users', (req,res,next)=>{
-    res.render('users', {pageTitle:'users', users:users})
+    // The blue users here should match what is used in the users.pug
+    res.render('users', {pageTitle:'User', users:users});
 });
 
-app.post('/add-user', (req,res,next)=>{
+app.post('/add-user',(req,res,next)=>{
     users.push({name:req.body.username});
-    res.redirect('/users')
+    res.redirect("/users");
+});
 
-})
-
-
-
+// app.post('/add-user', (req, res, next) => {
+//     users.push({ name: req.body.username });
+//     res.redirect('/users');
+//   });
+  
 
 app.listen(5000)
