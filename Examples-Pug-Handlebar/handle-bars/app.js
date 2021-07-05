6b65bc735cb5a72190645621d6a67f3a60edf66b
Jay Idrees <idreesjay@gmail.com>
2021-07-04 20:08:00 -0400

    Added configuration code for Hbs in app.js

--- Examples-Pug-Handlebar/handle-bars/app.js
+++ Examples-Pug-Handlebar/handle-bars/app.js
@@ -1,15 +1,27 @@
 const express=require('express');
 const bodyParser=require('body-parser')
+const hbs=require('express-handlebars');
 
 
 
 const app=express();
+app.engine('hbs',hbs({defaultLayout:'main-layouts', extname:'hbs'}))
+app.set('views','views')
 
 app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+
 
 app.get('/',(req,res,next)=>{
     console.log("Testing the get route")
 })
+
+
+
+
+
 
 
 app.listen(5000, console.log('http://localhost:5000'))
