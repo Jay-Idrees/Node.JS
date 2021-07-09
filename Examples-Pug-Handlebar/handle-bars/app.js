f6e3deddae78d259fc90d21226ece30e479c98b8
Jay Idrees <idreesjay@gmail.com>
2021-07-08 22:39:39 -0400

    Updated app.js

--- Examples-Pug-Handlebar/handle-bars/app.js
+++ Examples-Pug-Handlebar/handle-bars/app.js
@@ -1,23 +1,27 @@
 const express=require('express');
 const bodyParser=require('body-parser')
-const hbs=require('express-handlebars');
+const expressHbs=require('express-handlebars');
 
-
+const users=[];
 
 const app=express();
-app.engine('hbs',hbs({defaultLayout:'main-layouts', extname:'hbs'}))
-app.set('views','views')
+app.engine('hbs',expressHbs({defaultLayout:'main-layouts', extname:'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.get('/',(req,res,next)=>{
+    res.render('index', {pageTitle:'Add user'})
     
 });
 
-app.get('/users', (req,res,req)=>{
+app.get('/users', (req,res,req)=>{ 
 
 });
+
+
 
 app.post('/add-user', (req,res,next)=>{
 
