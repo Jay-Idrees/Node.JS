eac3d89746436704b96f8b85778384a3e638dfd7
Jay Idrees <idreesjay@gmail.com>
2021-07-10 18:37:04 -0400

    Fixed some errors and updated main-layout.hbs file

--- Examples-Pug-Handlebar/handle-bars/app.js
+++ Examples-Pug-Handlebar/handle-bars/app.js
@@ -7,19 +7,19 @@ const users=[];
 const app=express();
 
 app.engine('hbs',expressHbs({defaultLayout:'main-layouts', extname:'hbs'}));
-// app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.get('/',(req,res,next)=>{
-    res.render('index', {pageTitle:'Add user'})
+    res.render('index', {pageTitle:'Add user'});
     
 });
 
 app.get('/users', (req,res,next)=>{ 
-    res.render('users', {pageTitle:'Users', users:users, hasUsers:users.length>0}, )
+    res.render('users', {pageTitle:'Users', users:users, hasUsers:users.length>0});
 });
 
 
@@ -29,10 +29,6 @@ app.post('/add-user', (req,res,next)=>{
  res.redirect('/users')
 
 });
-
-
-
-
 
 
 
